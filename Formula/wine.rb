9b4013b590fe0b2963573e2a11be5fce011331ea
rogerdpack <rogerpack2005@gmail.com>
2019-12-18 07:24:10 -0700

    take 3

--- Formula/wine.rb
+++ Formula/wine.rb
@@ -17,10 +17,10 @@ class Wine < Formula
 
   # High Sierra doesn't support 32-bit builds, and thus wine fails to compile.
   # This will only be safe to remove when upstream support 64-bit builds.
-  depends_on :maximum_macos => [:sierra, :build]
+  #depends_on :maximum_macos => [:sierra, :build]
 
   depends_on "pkg-config" => :build
-  depends_on :macos => :el_capitan
+  depends_on :macos => :el_capitan # at least el capitan
 
   resource "mono" do
     url "https://dl.winehq.org/wine/wine-mono/4.7.5/wine-mono-4.7.5.msi"
